4c60381ceff64da40693f3a7100c5148c801a432
zitian <zitianchen01@gmail.com>
2023-07-12 23:04:49 -0700

    Add project files.

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products
+        { get; set; }
+    
+    }
+}
