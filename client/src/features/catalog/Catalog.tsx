11c37b3cebb331b87cc35be4056cd6fec485c456
zitian <zitianchen01@gmail.com>
2023-08-02 17:51:07 -0700

    React basic done

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,19 @@
+import { Fragment, useEffect, useState } from "react"
+import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
+
+export default function Catalog() {
+	const [products, setProducts] = useState<Product[]>([]);
+
+	useEffect(() => {
+		fetch('http://localhost:5081/api/products')
+			.then(response => response.json())
+			.then(data => setProducts(data))
+	}, [])
+
+	return (
+		<>
+			<ProductList products={products} /> 
+		</>
+    )
+}
---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,42 @@
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
+import { Product } from "../../app/models/product";
+
+interface Props {
+    product: Product;
+}
+
+export default function ProductCard({ product }: Props) {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+                avatar={
+                    <Avatar>
+                        {product.name.charAt(0).toLocaleUpperCase()}
+                    </Avatar>
+                }
+                title={product.name}
+                titleTypographyProps={{
+                    sx: { fontWeight: 'bold', color: 'primary.main'}
+                }}
+            />
+
+            <CardMedia
+                sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
+                image={product.pictureUrl}
+                title="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom color='secondary' variant="h5">
+                    {(product.price/ 100).toFixed(2)}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {product.brand} / {product.type}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Add to Card</Button>
+                <Button size="small">View</Button>
+            </CardActions>
+        </Card>
+    )
+}
